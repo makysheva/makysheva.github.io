c4bf6e5225c5b13d84bcc2477442dec22e26d604
makysheva <makysheva315181@gmail.com>
2019-07-23 07:26:53 +0600

    фильтрация портфолио

--- app/js/index.js
+++ app/js/index.js
@@ -1,4 +1,27 @@
- // Показать меню при клике на иконку
+// Фильтрация в портфолио
+
+(function () {
+    const filterBtn = document.querySelectorAll('.btn')
+    const portfolio = document.querySelectorAll('.portfolio-item')
+
+    filterBtn.forEach((btn) => {
+        btn.addEventListener('click', function() {
+            let attrDataTypeView = btn.getAttribute('data-type')
+
+            portfolio.forEach((portfolioItem) => {
+                let filterDataType = portfolioItem.getAttribute('data-type')
+
+                if(filterDataType === attrDataTypeView) {
+                    portfolioItem.classList.add('portfolio-item_hidden');
+                } else {
+                    portfolioItem.classList.remove('portfolio-item_hidden');
+                }
+            })
+        })
+    })
+})();
+
+// Показать меню при клике на иконку
 $('.mobile-menu').on('click', function(event) {
     event.preventDefault();
     $('.menu-wrap').toggleClass('menu-active')
