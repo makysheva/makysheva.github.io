95c1d16c797d5cc22d1dde8186d52997817851f9
makysheva <makysheva315181@gmail.com>
2019-07-29 16:19:18 +0600

    site electronpv

--- app/js/index.js
+++ app/js/index.js
@@ -5,18 +5,36 @@
     const portfolio = document.querySelectorAll('.portfolio-item')
 
     filterBtn.forEach((btn) => {
-        btn.addEventListener('click', function() {
+
+        btn.addEventListener('click', () => {
             let attrDataTypeView = btn.getAttribute('data-type')
 
             portfolio.forEach((portfolioItem) => {
                 let filterDataType = portfolioItem.getAttribute('data-type')
-
+                
                 if(filterDataType === attrDataTypeView) {
                     portfolioItem.classList.add('portfolio-item_hidden');
                 } else {
                     portfolioItem.classList.remove('portfolio-item_hidden');
                 }
             })
+        })
+    })
+})();
+
+// Закрытие меню при клике на пункт меню
+(function () {
+    const menuItems = document.querySelectorAll('.menu-item')
+    const menuWrap = document.querySelectorAll('.menu-wrap')
+
+    menuItems.forEach((menuItem) => {
+        menuItem.addEventListener('click', () => {
+            menuWrap.forEach((wrap) => {
+
+                if(wrap)
+                wrap.style.display = "none"
+                
+            })     
         })
     })
 })();
