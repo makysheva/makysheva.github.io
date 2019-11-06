85d9c1eef329cb95dbfa8700785a996c0fc5fda0
makysheva <makysheva.aliya@gmail.com>
2019-11-07 00:36:39 +0600

    логотип в header

--- rizzoma/webpack.config.js
+++ rizzoma/webpack.config.js
@@ -2,27 +2,39 @@ const path = require('path')
 const HtmlWebpackPlugin = require('html-webpack-plugin')
 const PATHS = {
     source: path.join(__dirname, 'src'),
-    build: path.join(__dirname, 'dist')
+    build: path.join(__dirname, 'dist'),
 };
+const PAGES_DIR = `${PATHS.source}/cards/`
 
 module.exports = {
-    entry: PATHS.source + '/index.js',
+    entry: {
+        'header': PATHS.source + '/cards/header/header.js',
+        'footer': PATHS.source + '/cards/footer/footer.js'
+    },
     output: {
         path: PATHS.build,
-        filename: '[name].js'
+        filename: '[name].js',
+        publicPath: '/'
     },
     plugins: [
         new HtmlWebpackPlugin({
-            template: PATHS.source + '/index.pug'
-        })
+            template: `${PAGES_DIR}/header/header.pug`,
+            filename: './dist/header.html',
+            inject: true
+          }),
+          new HtmlWebpackPlugin({
+            template: `${PAGES_DIR}/footer/footer.pug`,
+            filename: './dist/footer.html',
+            inject: true
+          })
     ],
     module: {
         rules: [{
             test: /\.js$/,
             loader: 'babel-loader',
             exclude: '/node_modules/'
-        }],
-        rules: [{
+        },
+        {
             test: /\.pug$/,
             loader: 'pug-loader',
             options: {
