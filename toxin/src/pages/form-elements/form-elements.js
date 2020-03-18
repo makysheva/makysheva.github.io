62195a44560e33e1d35ac682b311dc4de8d501e4
makysheva <makysheva.aliya@gmail.com>
2020-03-18 21:42:06 +0600

    закончила стилизацию dropdown в  form elements

--- toxin/src/pages/form-elements/form-elements.js
+++ toxin/src/pages/form-elements/form-elements.js
@@ -1,3 +1,28 @@
-import './form-elements.scss';
+import './form-elements.scss'
+
 
 console.log('in form-elements.js');
+
+
+const dropdownSelect = document.querySelectorAll('.form-elements__input--dropdown')
+const dropdownOpen = document.querySelectorAll('.dropdown__menu')
+
+
+dropdownSelect.forEach((elem) => {//нашел нужный div
+    elem.addEventListener('click', () => {// при клике на div
+
+        dropdownOpen.forEach((elemDropdown)=> {// добавлять класс
+            elemDropdown.classList.add('dropdown__menu--open')
+        })
+    })    
+})
+
+dropdownSelect.forEach((elem) => {//нашел нужный div
+    elem.removeEventListener('click', () => {// убрать слушатель
+            
+        dropdownOpen.forEach((elemDropdown)=> {// добавлять класс
+            elemDropdown.classList.remove('dropdown__menu--open')
+        })
+    })
+})
+
