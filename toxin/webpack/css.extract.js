fc6dd63e95fae10ee484a00d2637c5ed74c46c5b
makysheva <makysheva.aliya@gmail.com>
2020-03-25 14:38:36 +0600

    правка путей

--- toxin/webpack/css.extract.js
+++ toxin/webpack/css.extract.js
@@ -12,6 +12,7 @@ module.exports = function(){
                         loader: MiniCssExtractPlugin.loader,
                         options: {
                           hmr: process.env.NODE_ENV === 'development',
+                          publicPath: '../../',
                         },
                       },
                       'css-loader',
