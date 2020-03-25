6cf3fafe854eb5ecc45ed1c6a329374dbb6e0289
makysheva <makysheva.aliya@gmail.com>
2020-03-25 14:23:11 +0600

    добавлен Readme.md

---
+++ toxin/README.md
@@ -0,0 +1,33 @@
+# FSD Template
+Верстка шаблона ([Figma](https://www.figma.com/file/MumYcKVk9RkKZEG6dR5E3A/FSD-education-program.-The-2nd-task)) и UIKit в рамках тестового задания.
+
+Установка проекта ```npm i```
+
+Запуск сервера ```npm run start```
+
+Сборка проекта ```npm run build```
+
+
+Используемые библиотеки:
+- webpack
+- pug
+- scss
+
+## UI Kit
+[Colors & Type](http://makysheva.github.io/colors-type.html)
+
+[Headers & Footers](http://makysheva.github.io/header.html)
+
+[Form elements](http://makysheva.github.io/form-elements.html)
+<!-- 
+## Pages
+
+[Landing](http://makysheva.github.io/fsd/landing.html)
+
+[Registration](http://makysheva.github.io/fsd/registration.html)
+
+[Sign In](http://makysheva.github.io/fsd/sign-in.html)
+
+[Search room](http://makysheva.github.io/fsd/search-room.html)
+
+[Room details](http://makysheva.github.io/fsd/room-details.html) -->
--- toxin/src/pages/form-elements/form-elements.js
+++ toxin/src/pages/form-elements/form-elements.js
@@ -4,25 +4,53 @@ import './form-elements.scss'
 console.log('in form-elements.js');
 
 
-const dropdownSelect = document.querySelectorAll('.form-elements__input--dropdown')
-const dropdownOpen = document.querySelectorAll('.dropdown__menu')
+// const dropdownSelect = document.querySelectorAll('.form-elements__input--dropdown')
+// const dropdownOpen = document.querySelectorAll('.dropdown__menu')
 
 
-dropdownSelect.forEach((elem) => {//нашел нужный div
-    elem.addEventListener('click', () => {// при клике на div
+// dropdownSelect.forEach((elem) => {//нашел нужный div
+//     elem.addEventListener('click', () => {// при клике на div
 
-        dropdownOpen.forEach((elemDropdown)=> {// добавлять класс
-            elemDropdown.classList.add('dropdown__menu--open')
+//         dropdownOpen.forEach((elemDropdown)=> {// добавлять класс
+//             elemDropdown.classList.add('dropdown__menu--open')
+//         })
+//     })    
+// })
+
+
+//функция для октрытия/закрытия dropdown 
+    (function () {
+        const dropdownSelect = document.querySelectorAll('.form-elements__input--dropdown')
+        const dropdownOpen = document.querySelectorAll('.dropdown__menu')
+
+        dropdownSelect.forEach((elem) => {//нашел нужный div
+            elem.addEventListener('click', () => {// при клике на div
+        
+                dropdownOpen.forEach((elemDropdown)=> {// добавлять класс
+                    elemDropdown.classList.add('dropdown__menu--open')
+                })
+            })    
         })
-    })    
-})
 
-dropdownSelect.forEach((elem) => {//нашел нужный div
-    elem.removeEventListener('click', () => {// убрать слушатель
-            
-        dropdownOpen.forEach((elemDropdown)=> {// добавлять класс
-            elemDropdown.classList.remove('dropdown__menu--open')
+        dropdownSelect.forEach((elem) => {//нашел нужный div
+            elem.removeEventListener('click', () => {// при клике на div
+        
+                dropdownOpen.forEach((elemDropdown)=> {// добавлять класс
+                    elemDropdown.classList.remove('dropdown__menu--open')//не работает
+                })
+            })    
         })
+
+    })();
+
+// функция для увеличения/уменьшения выбора количества гостей
+
+const minusBtn = document.querySelectorAll('.dropdown__minus')
+
+
+
+Array.from(minusBtn).forEach(minusButtons => {
+    minusButtons.addEventListener('click', function (minusButton) {
+        minusButton
     })
 })
-
