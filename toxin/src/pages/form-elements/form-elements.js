a798b65d3a0f13535d8d745890ed62c0f966e2ee
makysheva <makysheva.aliya@gmail.com>
2020-05-27 00:31:36 +0600

    сверстан bullet-list

--- toxin/src/pages/form-elements/form-elements.js
+++ toxin/src/pages/form-elements/form-elements.js
@@ -21,22 +21,6 @@ import '../../blocks/form-elements/slider/slider'
 import '../../blocks/form-elements/slider/slider.scss'
 import '../../blocks/form-elements/buttons/buttons.scss'
 import '../../blocks/form-elements/pagination/pagination.scss'
+import '../../blocks/form-elements/bullet-list/bullet-list.scss'
 
 console.log('in form-elements.js');
-
-
-// const dropdownSelect = document.querySelectorAll('.form-elements__input--dropdown')
-// const dropdownOpen = document.querySelectorAll('.dropdown__menu')
-
-
-// dropdownSelect.forEach((elem) => {//нашел нужный div
-//     elem.addEventListener('click', () => {// при клике на div
-
-//         dropdownOpen.forEach((elemDropdown)=> {// добавлять класс
-//             elemDropdown.classList.add('dropdown__menu--open')
-//         })
-//     })
-// })
-
-
-
