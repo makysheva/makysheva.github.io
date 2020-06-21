606e3056f40ba984c8ec9c663cda8e35b477c38d
makysheva <makysheva.aliya@gmail.com>
2020-06-21 16:52:32 +0600

    сверстана карточка регистрации аккаунта

--- toxin/src/pages/cards/cards.js
+++ toxin/src/pages/cards/cards.js
@@ -8,6 +8,7 @@ import '../../blocks/form-elements/dropdown/dropdown.scss'
 import '../../blocks/form-elements/buttons/buttons.scss'
 import '../../blocks/form-elements/text-field/text-field.scss'
 import '../../blocks/form-elements/radio-btn/radio-btn.scss'
+import '../../blocks/form-elements/toggle/toggle.scss'
 
 
 console.log('in cards.js')
