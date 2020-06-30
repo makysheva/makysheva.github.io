a72e90dd6fea3df7f991c3fd8bdab06529ae87d4
makysheva <makysheva.aliya@gmail.com>
2020-06-30 21:11:21 +0600

    сделаны headers

--- toxin/src/pages/headers-footers/headers-footers.js
+++ toxin/src/pages/headers-footers/headers-footers.js
@@ -2,3 +2,4 @@ import './headers-footers.scss';
 import '../../blocks/headers-footers/menu/menu.scss'
 import '../../blocks/form-elements/buttons/buttons.scss'
 import '../../blocks/headers-footers/menu/menu'
+import '../../blocks/headers-footers/header/header.scss'
