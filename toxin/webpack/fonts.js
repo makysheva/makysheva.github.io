3236ed6f55604569177f312c6f4e515f875e890e
makysheva <makysheva.aliya@gmail.com>
2020-07-07 11:48:52 +0600

    правка путей до картинки в landing

--- toxin/webpack/fonts.js
+++ toxin/webpack/fonts.js
@@ -8,7 +8,8 @@ module.exports = function(){
                     test: /\.(woff(2)?|ttf|eot|svg)(\?v=\d+\.\d+\.\d+)?$/,
                     loader: 'file-loader',
                     options: {
-                      name: '[name].[ext]'
+                      name: '[name].[ext]',
+                      outputPath: 'assets/fonts/'
                     }
                 },
             ]
