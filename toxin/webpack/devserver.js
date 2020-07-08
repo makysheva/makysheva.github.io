48be1f76af6bdb8e1a2e6d41e9d11c26b7711214
makysheva <makysheva.aliya@gmail.com>
2020-07-08 15:53:43 +0600

    правка путей

--- toxin/webpack/devserver.js
+++ toxin/webpack/devserver.js
@@ -4,7 +4,7 @@ module.exports = function(){
     return{
         devServer: {
             stats: 'errors-only',
-            contentBase: path.resolve(__dirname, 'build'),
+            //contentBase: path.resolve(__dirname, 'build'),
             port: 9000,
         }
     }
--- toxin/webpack/images.js
+++ toxin/webpack/images.js
@@ -9,7 +9,7 @@ module.exports = function(){
                     loader: 'file-loader',
                     options: {
                         name: '[name].[ext]',
-                        publicPath: './assets/'
+                        publicPath: './assets/images/',
                     },
                 },
             ]
