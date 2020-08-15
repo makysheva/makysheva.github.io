afec0fdc2c1b818b5d1cf489ac23cb6c2b126595
makysheva <makysheva.aliya@gmail.com>
2020-08-15 20:40:07 +0600

    попытки правки чекбоксов expander в form elements

--- toxin/src/blocks/form-elements/expander/expander.js
+++ toxin/src/blocks/form-elements/expander/expander.js
@@ -1,19 +1,22 @@
 /* открытие/закрытие листа expander */
+(function(){
+    const expanderItems = document.querySelectorAll('.js-expander')
+    const expanderList = document.querySelectorAll('.expander__list')
 
-const expanderItems = document.querySelectorAll('.js-expander')
-const expanderList = document.querySelectorAll('.expander__list')
-
-expanderItems.forEach((expanderItem) => {
-    expanderItem.addEventListener('click', () => {
-        expanderList.forEach((expander) => {
-            expander.classList.toggle('expander__list_hided')
+    expanderItems.forEach((expanderItem) => {
+        expanderItem.addEventListener('click', () => {
+            expanderList.forEach((expander) => {
+                expander.classList.toggle('expander__list_hided')
+            })
         })
     })
-})
+})();
+
 
 /* переключение галочек на списке expander */
 const markBreakfast = document.getElementById('mark-breakfast')
 const markBreakfastOpen = document.getElementById('mark-breakfast--open')
+const markBreakfastOpens = document.getElementById('mark-breakfast--opens')
 const markDesk = document.getElementById('mark-desk')
 const markFeedingChair = document.getElementById('mark-feeding-chair')
 const markCot = document.getElementById('mark-cot')
@@ -30,6 +33,9 @@ checkboxHide.forEach((checkbox) => {
         }
         if(checkboxAttr === 'expander-breakfast--open'){
             markBreakfastOpen.classList.toggle('checkbox__mark--checked')
+        }
+        if(checkboxAttr === 'expander-breakfast--opens'){
+            markBreakfastOpens.classList.toggle('checkbox__mark--checked')
         }
         if(checkboxAttr === 'expander-desk'){
             markDesk.classList.toggle('checkbox__mark--checked')
